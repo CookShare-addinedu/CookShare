2f13bf9ae34abf12c38ec122349eea31f1f1e79f
5hyunsu <water.steam.cloud.rain@gmail.com>
2024-04-08 08:26:25 +0900

    ✨ 04_08_회원가입 DB 입력
    mySql 연동해서 회원가입하면 회원정보 테이블 저장

--- server/src/main/java/com/example/foodshare/service/UserService.java
+++ server/src/main/java/com/example/foodshare/service/UserService.java
@@ -1,28 +1,18 @@
 package com.example.foodshare.service;
 
 import com.example.foodshare.domain.User;
-import com.example.foodshare.dto.EnrollUser;
-import com.example.foodshare.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
-    @Autowired
-    UserRepository userRepository;
+import java.util.List;
+import java.util.Optional;
 
-    /*비번 암호화*/
-     @Autowired
-    BCryptPasswordEncoder bCryptPasswordEncoder;
-    public static void userEnroll(EnrollUser enrollUser) {
-        User data = new User();  /*DB에 있는 엔티티르르 엔롤로*/
-        data.setUsername(enrollUser.getUsername());
-        data.setPassword(bCryptPasswordEncoder.encode(enrollUser.getPassword()));
-        /*data.setRole("ROLE_ADMIN");*/
-        /*data.setRole("ROLE_USER");*/
 
-        UserRepository.save(data);
-    }
+public interface UserService {
+    Optional<User> findByMobileNumber(String mobileNumber);
+    User registerUser(User user);
+
+    List<User> findAllUsers();
+
+    boolean checkPassword(String rawPassword, String encodedPassword);
+
+    boolean checkNicknameDuplicate(String nickname);
 }
-
