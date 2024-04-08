8239f9c07c8fe450e9d11340681e03d1026d43a5
kangmin <lnewgatel@gmail.com>
2024-04-08 15:15:14 +0900

    feat: 이미지 저장 처리 구현

---
+++ server/src/main/java/com/foodshare/exception/FileStorageException.java
@@ -0,0 +1,7 @@
+package com.foodshare.exception;
+
+public class FileStorageException extends Exception {
+	public FileStorageException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
