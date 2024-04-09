e98104fdd8a41f7434766285b20051920a529f4a
An <aaapple@naver.com>
2024-04-09 18:33:14 +0900

    refactor: 리액트chat.jsx 로직 나눔

--- client/src/views/Chat/Chat.jsx
+++ client/src/views/Chat/Chat.jsx
@@ -1,101 +1,90 @@
 import React, {useEffect, useState} from "react";
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
-import './Chat.scss'
-import { jwtDecode } from 'jwt-decode';
-import axios from "axios";
+import './Chat.scss';
+import {jwtDecode} from 'jwt-decode';
+import useMessageList from "./useMessageList";
 
 function Chat() {
     const [stompClient, setStompClient] = useState(null);
-    const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState("");
 
     const token = localStorage.getItem('token');
     const decoded = jwtDecode(token);
     const firstUser = decoded.uid;
 
-    const secondUser = "01012345678";
-  //  const currentChatRoomId = [firstUser, secondUser].sort().join("_");
-    const currentChatRoomId = "01000000000_01012345678";
+    const currentChatRoomId = "01012345678_01012345678";
+
+
+    //const [messageList, setMessageList] = useMessageList(currentChatRoomId, firstUser);
+    const {messageList, addMessageList } = useMessageList(currentChatRoomId, firstUser);
 
     useEffect(() => {
-
         if (stompClient) {
             return;
         }
-
         const socket = new SockJS('http://localhost:8080/ws');
         const client = Stomp.over(socket);
 
-        client.connect({}, (frame) => {
-            console.log('연결정보: ', frame);
-
-            client.subscribe(`/topic/chat/room/${currentChatRoomId}`, message => {
+        client.connect({}, () => {
+            console.log('웹소켓 연결');
+            client.subscribe(`/topic/chat/room/${currentChatRoomId}`, (message) => {
                 const receivedMessage = JSON.parse(message.body);
 
-                setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+                addMessageList (receivedMessage);
             });
 
             setStompClient(client);
         }, (error) => {
-            console.error('연결에러 ', error);
+            console.error('웹소켓 연결에러: ', error);
         });
 
-        axios.get(`/api/detailRoom/${currentChatRoomId}`, {
-            params: {
-                userId: firstUser
-            }
-        })
-            .then(response => {
-                const data = response.data;
-                setMessages(data.content || []);
-                console.log("채팅조회!!!!!!!!!!!!!!!!!!!!", data);
-            })
-            .catch(error => {
-                console.error("채팅방 상세 정보 조회 에러: ", error);
-            });
 
+    }, [currentChatRoomId, addMessageList ]);
+
+
+    useEffect(() => {
         return () => {
-            if (client && client.connected) {
-                client.disconnect(() => {
-                    console.log("연결끊김");
+            if (stompClient && stompClient.connected) {
+                stompClient.disconnect(() => {
+                    console.log("웹소켓 끊김");
                 });
             }
         };
-    }, [currentChatRoomId]);
+    }, [stompClient]);
 
     const sendMessage = () => {
         if (stompClient && stompClient.connected && newMessage.trim() !== "") {
-            const currentTime = new Date().toISOString();
             const chatMessage = {
                 chatRoomId: currentChatRoomId,
                 sender: firstUser,
                 content: newMessage,
-                timestamp: currentTime
+                timestamp: new Date().toISOString()
             };
 
             stompClient.send(`/app/chat.room/${currentChatRoomId}/sendMessage`, {}, JSON.stringify(chatMessage));
-            console.log(chatMessage)
             setNewMessage("");
         } else {
-            console.log("메시지를 보낼수 없습니돠!! 웹소켓 연결확인하고 또는 메시지 확인");
+            console.log("메시지를 전송할수 없습니다 : Chat.jsx sendMessage");
         }
     };
+
 
     return (
         <div className="chatForm">
             <div className="messagesContainer">
-                {messages.map((msg, index) => (
+                {messageList.map((msg, index) => (
                     <div
                         className={`chatContent ${msg.sender === firstUser ? "me" : "them"}`}
                         key={index}
                     >
-                        {msg.sender !== firstUser && <img src="/images/userImage.png" alt={`${msg.sender}`} />}
+                        {msg.sender !== firstUser && <img src="/images/userImage.png" alt={`${msg.sender}`}/>}
                         <div className="messageArea">
                             <div className="messageInfo">
                                 <div className="sender">{msg.sender === firstUser ? `` : msg.sender}</div>
                                 <div className="content">{msg.content}</div>
-                                <div className="timestamp">{msg.timestamp ? new Date(msg.timestamp).toLocaleString() : 'N/A'}</div>
+                                <div
+                                    className="timestamp">{msg.timestamp ? new Date(msg.timestamp).toLocaleString() : 'N/A'}</div>
                             </div>
                         </div>
                     </div>
@@ -115,3 +104,7 @@ function Chat() {
 }
 
 export default Chat;
+
+
+
+
