b2abdfe05f40befcee669ba3b498749445f3ba52
An <aaapple@naver.com>
2024-04-19 19:31:09 +0900

    Feat: Chat Front _ 목록조회 , Stomp연결

--- client/src/router.js
+++ client/src/router.js
@@ -1,14 +1,32 @@
 import Login from "./views/auth/login/Login";
 import {Header1, Header2, Header3, Header4, Header5} from "./views/common/header/Header";
 import Footer from "./views/common/footer/Footer";
+import Chat from "./views/Chat/Chat";
+import ChatRoomList from "./views/Chat/ChatRoomList";
+
 const router = {
     routes: [
         {
             path: '/login',
             component: <Login/>,
-            header:<Header2/>,
-            footer:null
+            header: <Header2/>,
+            footer: null
         },
+        {
+            path: '/chat/GetChatList',
+            component: <ChatRoomList/>,
+            header: <Header5/>,
+            footer: <Footer/>
+        },
+
+        {
+            path: '/chat/GetChat/:chatRoomId',
+            component: <Chat/>,
+            header: <Header5/>,
+            footer: <Footer/>
+        }
+
+
     ]
 }
 export default router;
