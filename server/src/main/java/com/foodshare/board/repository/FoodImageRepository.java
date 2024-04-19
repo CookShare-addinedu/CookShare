1beebf75d65a06aeb94d28a84206b62ac8454c2e
kangmin <lnewgatel@gmail.com>
2024-04-19 11:39:40 +0900

    feat: food read, list기능 구현 저장 값 없으면 예외 처리 이미지 업로드를 위한 파일 이미지 접근

--- server/src/main/java/com/foodshare/board/repository/FoodImageRepository.java
+++ server/src/main/java/com/foodshare/board/repository/FoodImageRepository.java
@@ -1,9 +1,12 @@
 package com.foodshare.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.foodshare.domain.FoodImage;
 @Repository
 public interface FoodImageRepository extends JpaRepository<FoodImage, Long> {
+	List<FoodImage> findByFoodFoodId(Long foodId);
 }
--- server/src/main/java/com/foodshare/board/repository/FoodRepository.java
+++ server/src/main/java/com/foodshare/board/repository/FoodRepository.java
@@ -1,5 +1,7 @@
 package com.foodshare.board.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.foodshare.domain.Food;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
+	List<Food> findAll();
 }
