c3bbe49e5be23fb27b57f740efb5cc66fd64adbe
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-04-22 10:50:12 +0900

    feat: OnBoarding page 추가

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Login from "./views/auth/login/Login";
 import {Header1, Header2, Header3, Header4, Header5} from "./views/common/header/Header";
 import Footer from "./views/common/footer/Footer";
+import OnBoarding from "./views/pages/onboarding/OnBoarding";
 const router = {
     routes: [
         {
@@ -9,6 +10,12 @@ const router = {
             header:<Header2/>,
             footer:null
         },
+        {
+            path: '/',
+            component: <OnBoarding/>,
+            header:null,
+            footer:null
+        }
     ]
 }
 export default router;
