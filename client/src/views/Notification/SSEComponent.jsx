57d02199f1eab10860e007c2e567432117802ecd
An <aaapple@naver.com>
2024-04-22 14:38:45 +0900

    fix: 띄어쓰기  설정

--- client/src/views/Notification/SSEComponent.jsx
+++ client/src/views/Notification/SSEComponent.jsx
@@ -1,1 +1,31 @@
-import React, {useEffect} from 'react';import useSSEConnection from "./useSSEConnection";import NotificationMessage from "./NotificationMessage";const SSEComponent = () => {    const {messages, isConnected,setMessages} = useSSEConnection();    useEffect(() => {        console.log("SSE Messages:", messages); // 상태 업데이트 확인    }, [messages]);    return (        <div>            {isConnected ? (                <>                    {messages.map((msg, index) => (                        <NotificationMessage                            key={index}                            message={msg.message} /* 알림 메시지 표시 */                            onClose={() => setMessages((prev) => prev.filter((_, i) => i !== index))} /* 닫기 */                        />                    ))}                </>            ) : (                <div></div>             )}        </div>    );};export default SSEComponent;
+import React, {useEffect} from 'react';
+import useSSEConnection from "./useSSEConnection";
+import NotificationMessage from "./NotificationMessage";
+
+
+const SSEComponent = () => {
+    const {messages, isConnected,setMessages} = useSSEConnection();
+
+    useEffect(() => {
+        console.log("SSE Messages:", messages); // 상태 업데이트 확인
+    }, [messages]);
+    return (
+        <div>
+            {isConnected ? (
+                <>
+                    {messages.map((msg, index) => (
+                        <NotificationMessage
+                            key={index}
+                            message={msg.message} /* 알림 메시지 표시 */
+                            onClose={() => setMessages((prev) => prev.filter((_, i) => i !== index))} /* 닫기 */
+                        />
+                    ))}
+                </>
+            ) : (
+                <div></div> 
+            )}
+        </div>
+    );
+};
+
+export default SSEComponent;
