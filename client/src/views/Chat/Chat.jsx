c5a9b987fe67d6502446881092f134c3f5e46215
AN-js <145098510+AN-js@users.noreply.github.com>
2024-04-25 14:45:35 +0900

    refactor:chat_fe 시큐리티 경로 적용및 , 데이터 전송 구조 변경

--- client/src/views/Chat/Chat.jsx
+++ client/src/views/Chat/Chat.jsx
@@ -1,16 +1,16 @@
 import React, {useCallback, useEffect, useRef, useState} from "react";
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
 import './Chat.scss';
 import {jwtDecode} from 'jwt-decode';
 import useChatDetailList from "./useChatDetailList";
 import {useParams} from "react-router-dom";
 import {useScrollManagement} from "./useScrollManagement";
+import useWebSocketConnection from "./useWebSocketConnection";
 
 
 function Chat() {
     const {chatRoomId} = useParams(); // URL에서 roomId 추출 . app.js ChatRoomList
-    const [stompClient, setStompClient] = useState(null);
+
+
     const [newMessage, setNewMessage] = useState("");
 
     const [isFirstLoaded, setIsFirstLoaded] = useState(true);
@@ -18,73 +18,44 @@ function Chat() {
     const [hasUserScrolled, setHasUserScrolled] = useState(false);
     const messagesContainerRef = useRef(null);
 
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('jwt');
     const decoded = jwtDecode(token);
-    const userId = decoded.uid;
+    const userId = decoded.mobilenumber;
 
     const {messageList, isLoading, loadChatMessages, hasMore, addMessageList} = useChatDetailList(chatRoomId, userId);
 
+    const stompClient = useWebSocketConnection(
+        "http://localhost:8080/ws",
+        `/topic/chat/room/${chatRoomId}`,
+        (receivedMessage) => {
+            addMessageList(receivedMessage);
+        }
+    );
     useScrollManagement(messagesContainerRef, messageList, hasMore, isLoading,
         loadChatMessages, isFirstLoaded, setIsFirstLoaded, lastMessageTimestamp,
         hasUserScrolled, setHasUserScrolled, userId);
 
 
     useEffect(() => {
-        if (stompClient) {
-            return;
-        }
-        const socket = new SockJS('http://localhost:8080/ws');
-        const client = Stomp.over(socket);
-
-        client.connect({}, () => {
-            console.log('웹소켓 연결');
-            client.subscribe(`/topic/chat/room/${chatRoomId}`,
-                (message) => {
-                    const receivedMessage = JSON.parse(message.body);
-                    addMessageList(receivedMessage);
-                });
-
-            setStompClient(client);
-        }, (error) => {
-            console.error('웹소켓 연결에러: ', error);
-        });
-
-    }, [chatRoomId, addMessageList]);
-
-    // 컴포넌트가 언마운트될 때 웹소켓 연결 해제
-    useEffect(() => {
-        return () => {
-            if (stompClient && stompClient.connected) {
-                stompClient.disconnect(() => {
-                    console.log("웹소켓 끊김");
-                });
-            }
-        };
-    }, [stompClient]);
-
-    useEffect(() => {
-        loadChatMessages();  // 컴포넌트 마운트 시 초기 메시지 로드
+        loadChatMessages();
     }, []);
 
 
     const sendMessage = () => {
-        if (stompClient && stompClient.connected && newMessage.trim() !== "") {
+        if (stompClient && stompClient.connected && newMessage.trim() !== '') {
             const timestamp = new Date().toISOString();
             const chatMessage = {
                 chatRoomId: chatRoomId,
                 sender: userId,
                 content: newMessage,
-                timestamp: timestamp
+                timestamp: timestamp,
             };
 
-            stompClient.send(`/app/chat.room/${chatRoomId}/sendMessage`, {},
-                JSON.stringify(chatMessage));
+            stompClient.send(`/app/chat.room/${chatRoomId}/sendMessage`, {}, JSON.stringify(chatMessage));
             setLastMessageTimestamp(timestamp);
-            setNewMessage("");
-
-
+            setNewMessage('');
         } else {
-            console.log("메시지를 전송할수 없습니다 : Chat.jsx sendMessage");
+            console.log("메시지를 전송할 수 없습니다.");
         }
     };
 
