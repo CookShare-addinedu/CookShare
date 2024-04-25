cce5cf62de1dd47e53c0ed12058984c7b8226e58
AN-js <145098510+AN-js@users.noreply.github.com>
2024-04-25 14:35:18 +0900

    fix : 문자오류

--- server/src/main/java/com/foodshare/chat/exception/DatabaseServiceUnavailableException.java
+++ server/src/main/java/com/foodshare/chat/exception/DatabaseServiceUnavailableException.java
@@ -1,6 +1,7 @@
 package com.foodshare.chat.exception;
 
 public class DatabaseServiceUnavailableException extends RuntimeException {
+	//
 	public DatabaseServiceUnavailableException(String message, Throwable cause) {
 		super(message, cause);
 	}
