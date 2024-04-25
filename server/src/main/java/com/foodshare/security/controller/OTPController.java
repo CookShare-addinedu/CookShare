b0ec25d585565014f7207af26c0ac038b2652a34
5hyunsu <water.steam.cloud.rain@gmail.com>
2024-04-25 09:57:14 +0900

    refactor: 문자인증 프론트 연동 확인 완료

--- server/src/main/java/com/foodshare/security/controller/OTPController.java
+++ server/src/main/java/com/foodshare/security/controller/OTPController.java
@@ -5,6 +5,7 @@ import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
 import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,9 +21,9 @@ import java.util.Random;
 public class OTPController {
     final DefaultMessageService messageService;
 
-    public OTPController() {
-       this.messageService = NurigoApp.INSTANCE.initialize("NCSIFI4ROLXOUATB", "S0IX1HZM9VHKKCNR2OW80BQNNHWPS9I1", "https://api.coolsms.co.kr");
-
+    public OTPController(@Value("${coolsms.api.key}") String apiKey,
+                         @Value("${coolsms.api.secret}") String apiSecret) {
+        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecret, "https://api.coolsms.co.kr");
     }
 
 
