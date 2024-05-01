f3e899448aa5bbf5ae59068f984da7f5777dad3f
AN-js <145098510+AN-js@users.noreply.github.com>
2024-05-01 22:10:50 +0900

    feat:be_chat 채팅방 생성

--- server/src/main/java/com/foodshare/chat/service/ChatRoomService.java
+++ server/src/main/java/com/foodshare/chat/service/ChatRoomService.java
@@ -22,6 +22,7 @@ import com.foodshare.chat.repository.ChatRoomRepository;
 import com.foodshare.domain.ChatMessage;
 import com.foodshare.domain.ChatRoom;
 import com.foodshare.chat.utils.ValidationUtils;
+import com.foodshare.domain.Notification;
 import com.foodshare.domain.User;
 import com.foodshare.notification.service.NotificationService;
 import com.foodshare.notification.sse.service.SseEmitterService;
@@ -42,17 +43,52 @@ public class ChatRoomService {
 	private final UserServiceImpl userService;
 	private final MongoQueryBuilder mongoQueryBuilder;
 
+	private final NotificationService notificationService;
+	private final SseEmitterService sseEmitterService;
+
 	@LogExecutionTime
 	public List<ChatRoomDto> listChatRoomsForUser(String userId) {
 		log.debug("listChatRoomsForUser: userId={}", userId);
-
 		Set<String> hiddenRoomIds = visibilityService.getHiddenRoomIds(userId);
-
 		List<ChatRoomDto> chatRooms = listAvailableChatRooms(userId, hiddenRoomIds);
-
 		updateUnreadCounts(chatRooms, userId);
-
 		return chatRooms;
+	}
+
+	public ChatRoom findOrCreateChatRoom(String firstUserId, String secondUserId, String foodId) {
+		log.info("채팅방 서비스 레이어에 들어왔습니다");
+
+		User firstUser = userService.findByMobileNumber(firstUserId)
+			.orElseThrow(() -> new IllegalStateException(firstUserId + " ID를 가진 사용자를 찾을 수 없습니다."));
+		User secondUser = userService.findByMobileNumber(secondUserId)
+			.orElseThrow(() -> new IllegalStateException(secondUserId + " ID를 가진 사용자를 찾을 수 없습니다."));
+
+		String chatRoomId = firstUserId + "_" + secondUserId;
+		String urlIdentifier = firstUser.getNickName() + "_" + secondUser.getNickName();
+
+		return findByUrlIdentifier(urlIdentifier)
+			.orElseGet(() -> createAndNotifyNewChatRoom(firstUserId, secondUserId, foodId, chatRoomId, urlIdentifier));
+	}
+
+	private List<ChatRoomDto> listAvailableChatRooms(String userId, Set<String> hiddenRoomIds) {
+		log.debug(" listAvailableChatRooms");
+		return chatRoomRepository.findByIdContaining(userId).stream()
+			.filter(room -> !hiddenRoomIds.contains(room.getUrlIdentifier()))
+			.map(this::convertRoomToDto)
+			.collect(Collectors.toList());
+	}
+
+	private ChatRoom createAndNotifyNewChatRoom(String firstUserId, String secondUserId, String foodId,
+		String chatRoomId, String urlIdentifier) {
+		ChatRoom newRoom = createChatRoom(firstUserId, secondUserId, foodId, chatRoomId, urlIdentifier);
+		createNotifyNewChatRoom(firstUserId, newRoom);
+		return newRoom;
+	}
+
+	private void createNotifyNewChatRoom(String userId, ChatRoom chatRoom) {
+		Notification notification = notificationService.createRoomCreationNotification(userId,
+			chatRoom.getSecondUser());
+		sseEmitterService.sendIndividualNotification(userId, notification);
 	}
 
 	private ChatRoomDto convertRoomToDto(ChatRoom room) {
@@ -72,14 +108,6 @@ public class ChatRoomService {
 		return chatDataMapper.toChatRoomDto(room, lastMessage);
 	}
 
-	private List<ChatRoomDto> listAvailableChatRooms(String userId, Set<String> hiddenRoomIds) {
-		log.debug(" listAvailableChatRooms");
-		return chatRoomRepository.findByIdContaining(userId).stream()
-			.filter(room -> !hiddenRoomIds.contains(room.getUrlIdentifier()))
-			.map(this::convertRoomToDto)
-			.collect(Collectors.toList());
-	}
-
 	private void updateUnreadCounts(List<ChatRoomDto> chatRooms, String userId) {
 		chatRooms.forEach(room -> {
 			long unreadCount = mongoQueryBuilder.countUnreadMessages(room.getChatRoomId(), userId);
@@ -88,8 +116,8 @@ public class ChatRoomService {
 		});
 	}
 
-	public ChatRoom createChatRoom(String firstUserId, String secondUserId, Long foodId, String chatRoomId,
-		String chaRoomUrlId) {
+	public ChatRoom createChatRoom(String firstUserId, String secondUserId, String foodId, String chatRoomId,
+		String urlIdentifier) {
 		log.debug("createChatRoom");
 		ValidationUtils.validateNotEmpty(firstUserId, "firstUserId");
 		ValidationUtils.validateNotEmpty(secondUserId, "secondUserId");
@@ -99,22 +127,10 @@ public class ChatRoomService {
 			.firstUser(firstUserId)
 			.secondUser(secondUserId)
 			.foodId(foodId)
-			.urlIdentifier(chaRoomUrlId)
+			.urlIdentifier(urlIdentifier)
 			.build();
 		log.debug("chatRoomId={}", chatRoom);
 		return chatRoomRepository.save(chatRoom);
-	}
-
-	public ChatRoom findOrCreateChatRoom(String firstUserId, String secondUserId, Long foodId) {
-		Optional<User> firstUserNickName = userService.findByMobileNumber(firstUserId);
-		Optional<User> secondUserNickName = userService.findByMobileNumber(secondUserId);
-		String chatRoomUrlId = firstUserNickName + "_" + secondUserNickName;
-
-		Optional<ChatRoom> existingChatRoom = findByUrlIdentifier(chatRoomUrlId);
-		return existingChatRoom.orElseGet(() -> {
-			String chatRoomId = firstUserId + "_" + secondUserId;
-			return createChatRoom(firstUserId, secondUserId, foodId, chatRoomId, chatRoomUrlId);
-		});
 	}
 
 	public ChatRoomCreationDto toChatRoomCreationDto(ChatRoom room) {
