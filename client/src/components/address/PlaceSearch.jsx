a3046aa23d6b6ce369c4df6e9bac6b8d7dda4609
eunjjang <83636742+eun-su-jeong@users.noreply.github.com>
2024-05-15 05:41:51 +0900

    feat: 스타일링 및 검색된 키워드 하이라이트 처리

--- client/src/components/address/PlaceSearch.jsx
+++ client/src/components/address/PlaceSearch.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import useKakaoLoader from './useKakaoLoader';
-import {Map, MapMarker} from "react-kakao-maps-sdk";
-
+import './PlaceSearch.scss';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLocationDot, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 const PlaceSearch = ({onLocationSelect}) => {
     const [inputText, setInputText] = useState('');
     const [places, setPlaces] = useState([]);
+    const [title, setTitle] = useState('');
 
     useKakaoLoader();
 
@@ -14,8 +16,10 @@ const PlaceSearch = ({onLocationSelect}) => {
             ps.keywordSearch(inputText, (data, status) => {
                 if (status === window.kakao.maps.services.Status.OK) {
                     setPlaces(data);
+                    setTitle('장소결과');
                 } else {
                     setPlaces([]);
+                    setTitle('검색 결과가 없습니다.');
                     alert('검색 결과가 없습니다.');
                 }
             });
@@ -35,20 +39,47 @@ const PlaceSearch = ({onLocationSelect}) => {
         onLocationSelect(locationInfo);
     };
 
+    const highlightText = (text, highlight) => {
+        if (!highlight) return text;
+        const parts = text.split(new RegExp(`(${highlight})`, 'gi'));
+        return parts.map((part, index) =>
+            part.toLowerCase() === highlight.toLowerCase() ? (
+                <span key={index} className="highlight">
+                    {part}
+                </span>
+            ) : (
+                part
+            )
+        );
+    };
+
     return (
-        <div>
-            <input
-                type="text"
-                value={inputText}
-                onChange={e => setInputText(e.target.value)}
-                placeholder="Search for a place"
-            />
-            <button onClick={searchPlaces}>Search</button>
+        <div className="place-search-wrap">
+            <div className="input-wrap">
+                <input
+                    type="text"
+                    value={inputText}
+                    onChange={e => setInputText(e.target.value)}
+                    placeholder="장소를 검색해주세요"
+                />
+                <button onClick={searchPlaces}>
+                    <FontAwesomeIcon className={'search'} icon={faMagnifyingGlass}/>
+                </button>
+            </div>
             <ul>
-                {places.map((place, index) => (
+            <p className={'title'}>{title}</p>
+            {places.map((place, index) => (
                     <li key={index}
-                        onClick={() => handlePlaceClick(place)}>
-                        {place.place_name} - {place.address_name}
+                        onClick={() => handlePlaceClick(place)}
+                        className={place.place_name.includes(inputText) && inputText !== '' ? 'matching-text' : ''}
+                    >
+                        <span className={'icon'}>
+                            <FontAwesomeIcon className={'dot'} icon={faLocationDot}/>
+                        </span>
+                        <div className={'info'}>
+                            <p>{highlightText(place.place_name, inputText)}</p>
+                            <span className={'address'}>{place.address_name}</span>
+                        </div>
                     </li>
                 ))}
             </ul>
